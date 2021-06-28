b12a288ae9f3204e6f87fb544018a3b661cfea7c
Bruno Gualberto <67048428+Bruno-Gualberto@users.noreply.github.com>
2021-06-28 22:16:45 +0200

    fixing Header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -8,17 +8,29 @@ import SideDrawer from './SideDrawer';
 const Header = () => {
 
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [headerScroll, setHeaderScroll] = useState(false);
 
   const toggleDrawer = (value) => {
     setDrawerOpen(value)
   }
 
+  const handleScroll = () => {
+    if(window.scrollY > 0) {
+      setHeaderScroll(true)
+    } else {
+      setHeaderScroll(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <AppBar
       position="fixed"
       style={{
-        backgroundColor: "#2f2f2f",
+        backgroundColor: headerScroll ? "#2f2f2f" : "transparent",
         boxShadow: "none",
         padding: "10px 0px"
       }}
